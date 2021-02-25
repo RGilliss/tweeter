64a324d7fa13b832778fd671141fbcc94ea485b9
Rye Gilliss <gilliss.rg@gmail.com>
2021-02-25 16:29:22 +0000

    functions added to pull tweets from database and post them to site, same goes for newly created tweets, error message also added if tweet does not meet specifications

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,100 @@
 /*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+* Client-side JS logic goes here
+* jQuery is already loaded
+* Reminder: Use (and do all your DOM work in) jQuery's document ready function
+*/
+
+const parser = function (tweet) {
+  let parsedTweet = tweet.slice(6);
+  parsedTweet = parsedTweet.split('');
+  parsedTweet.forEach(val => {
+    if (val === '%') {
+      let location = parsedTweet.indexOf('%');
+      if (parsedTweet[location + 1] === '2' && parsedTweet[location + 2] === '0') {
+        parsedTweet.splice(location, 3, " ")
+      }
+    }
+  });
+  
+  parsedTweet = parsedTweet.join('');
+  return parsedTweet;
+}
+
+//Creates the markup for the tweet
+const createTweetElement = function (tweetData) {
+  const tweetLayout = `
+  <article class="article-tweet">
+  <header>
+  <div>
+  <img src="${tweetData.user.avatars}">
+  <span>${tweetData.user.name}</span>
+  </div>
+  <a>${tweetData.user.handle}</a>
+  </header> 
+  <p>${tweetData.content.text}</p>
+  <footer>
+  <span>${tweetData.created_at}</span>
+  <i>🏴  🔄  💙</i>
+  </footer>
+  </article>`;
+  return tweetLayout;
+}
+
+//Loops through the database to and passes each to createTweetElement
+const renderTweets = function (data) {
+  // const $tweetContainer = $('.tweet-container');
+  // $tweetContainer.empty();
+  for (const user of data) {
+    $('.tweet-container').prepend(createTweetElement(user));
+  }
+};
+
+//loads data from the tweet page and passes them to renderTweets
+const loadTweets = function() {
+  $.ajax("/tweets", {method: 'GET'})
+  .then(function(data) {
+    renderTweets(data);
+  });
+};
+
+
+$(document).ready(function() {
+  loadTweets();
+  //error message
+  const error = document.getElementById('error')
+  error.style.display = "none";
+  
+  $("#new-tweet-form").submit(function(event) {
+    const $userTweet = $("#tweet-text")
+    let submission = $('#new-tweet-form').serialize();
+    
+    event.preventDefault();
+    if ($userTweet.val().length > 140) {
+      event.preventDefault();
+      error.textContent="Your tweet is too long";
+      error.style.display = "block";
+    }
+    if ($userTweet.val().length === 0) {
+      event.preventDefault();
+      error.textContent="You should type something first";
+      error.style.display = "block";
+    }
+    if ($userTweet.val().length <= 140 && $userTweet.val().length > 0) {
+      submission = parser(submission);
+      error.style.display = "none";
+      $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: {text: submission}
+      }).then(() => {
+        loadTweets();
+        error.style.display = "none";
+      }).catch(err => {
+        console.log('ERR caught in AJAX POST: ', err);
+      })
+    }
+});
+});  
+
+
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  //sets counter equal to 140 minus the length of the input in the text box
   document.getElementById("tweet-text").addEventListener("input", function () {
     const charCount = 140 - $(this).val().length;
     const counter = $(this).parent().find(".counter");
@@ -7,6 +8,8 @@ $(document).ready(function() {
   })
 });
 
+
+//Changes color of character count if above or below 0
 const colorChange = function (counter, charCount) {
   if (charCount < 0) {
     $(counter).css("color", "red")
